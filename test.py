170ea6c1497640520dd6ecb2a7ec9ca958c5d240
Oscar Lago <oscar.lago.uy@gmail.com>
2018-12-24 09:52:08 -0300

    fix test

--- test.py
+++ test.py
@@ -6,6 +6,11 @@ import time
 mpu = MPU9250()
 mpu.initialize()
 
-while True:
-    time.sleep(10)
-    print (mpu.get_avg().get_json())
+i = 0
+avg = mpu.get_avg()
+
+while avg.N == 0 and i < 5:
+    avg = mpu.get_avg()
+    i += 1
+
+print (avg.get_json())
